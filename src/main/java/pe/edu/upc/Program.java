e14a8b1eb3a40c58ece2bb75a0d932dead13c44c
Rogger Barrientos Mauricio <u201924159@upc.edu.pe>
2022-03-25 20:39:33 -0500

    Creacion de una clase y un objeto Student

---
+++ src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,15 @@
+package pe.edu.upc;
+
+import java.util.Iterator;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student();
+		student.setFirstName("Rogger BM");
+		System.out.println(student.getFirstName());
+	
+	}
+
+}
